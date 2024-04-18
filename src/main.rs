9e615187ac4d0d6fd1088fc803a73fc241fd60c1
Berke Kiran <berkekiranofficial@hotmail.com>
2024-04-19 00:48:48 +0300

    Initial commit based on ChainX v5.2.3

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// Copyright 2019-2023 ChainX Project Authors. Licensed under GPL-3.0.
+
+//! ChainX CLI
+
+fn main() -> cli::Result<()> {
+    cli::run()
+}
---
+++ xpallets/gateway/common/src/migrations/mod.rs
@@ -0,0 +1,5 @@
+// Copyright 2019-2023 ChainX Project Authors. Licensed under GPL-3.0.
+
+//! All migrations of this pallet.
+
+pub mod taproot;
